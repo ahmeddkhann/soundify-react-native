94d4b4afacddabf6110cf7af3675ff640a126540
ahmeddkhann <iamahmedkhan02@gmail.com>
2024-12-05 18:11:31 +0500

    data of all music is added

---
+++ app/constants.js
@@ -0,0 +1,50 @@
+export const playListData = [
+    {
+        id: 1,
+        title: "Maan Meri Jaan",
+        artist: "Arijit Singh",
+        album: "Champagne Talk",
+        artWork: "https://upload.wikimedia.org/wikipedia/en/7/77/King_-_Champagne_Talk.jpg", 
+        url: require("../assets/sounds/audio1.mp3"),
+    },
+    {
+        id: 2,
+        title: "Kesariya",
+        artist: "Arijit Singh",
+        album: "Brahmastra",
+        artWork: "https://upload.wikimedia.org/wikipedia/en/e/ec/Kesariya_song_cover.jpg",
+        url: require("../assets/sounds/audio2.mp3"),
+    },
+    {
+        id: 3,
+        title: "Raataan Lambiyan",
+        artist: "Jubin Nautiyal",
+        album: "Shershaah",
+        artWork: "https://upload.wikimedia.org/wikipedia/en/4/4e/Shershaah_album_cover.jpg",
+        url: require("../assets/sounds/audio3.mp3"),
+    },
+    {
+        id: 4,
+        title: "Tum Hi Ho",
+        artist: "Arijit Singh",
+        album: "Aashiqui 2",
+        artWork: "https://upload.wikimedia.org/wikipedia/en/9/9c/Aashiqui_2_Official_Album_Cover.jpg",
+        url: require("../assets/sounds/audio4.mp3"),
+    },
+    {
+        id: 5,
+        title: "Tera Yaar Hoon Main",
+        artist: "Arijit Singh",
+        album: "Sonu Ke Titu Ki Sweety",
+        artWork: "https://upload.wikimedia.org/wikipedia/en/5/55/Sonu_Ke_Titu_Ki_Sweety.jpg",
+        url: require("../assets/sounds/audio5.mp3"),
+    },
+    {
+        id: 6,
+        title: "Phir Aur Kya Chahiye",
+        artist: "Arijit Singh",
+        album: "Zara Hatke Zara Bachke",
+        artWork: "https://upload.wikimedia.org/wikipedia/en/1/1a/Zara_Hatke_Zara_Bachke_Poster.jpg",
+        url: require("../assets/sounds/audio6.mp3"),
+    }
+];
